f2abf2ed4b98c962b476c5b5b5140790e10d94fc
Preciousokeke25 <preciousokeke393@gmail.com>
2022-06-24 21:54:08 +0100

    first commit

---
+++ life.js
@@ -0,0 +1,17 @@
+const Name = document.getElementById("name");
+const Height= document.getElementById("height");
+const Country = document.getElementById("country");
+
+Name.innerText="Name:" + "Precious Okeke";
+Height.innerText="Height:" + "5.76m";
+Country.innerText="Country:" + "Nigeria";
+
+
+let myName ="precious Okeke"
+let myHeight ="5.76m"
+let myCountry ="Nigeria"
+
+
+alert('My Name is:${myName}');
+alert('My Height is:${myHeight}');
+alert('My Country Name is:${myCountry}');
